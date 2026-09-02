2fb5e7f0e49915860c81f5a34ec3d577a5576188
Shankar <skreddy040@gmail.com>
2026-09-02 08:30:58 -0400

    chore(web): embed F67 SAN review

--- internal/webui/dist/assets/check-BqQJ2OxA.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-CfpFSc1V.js";var t=e(`Check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- internal/webui/dist/assets/loader-circle-DVKtwBAe.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-CfpFSc1V.js";var t=e(`LoaderCircle`,[[`path`,{d:`M21 12a9 9 0 1 1-6.219-8.56`,key:`13zald`}]]);export{t};
